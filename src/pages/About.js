c6e4d85f1809608f3313ca980c45b31f48dd3392
Charles Bréton <charlesbreton@outlook.com>
2024-07-05 21:11:43 +0200

    fonctionnement

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Logo/>
+            <Navigation/>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam error alias expedita deserunt explicabo mollitia commodi, earum, minus dolores aspernatur quia vitae veritatis dolor est numquam nostrum eaque culpa iste neque iusto consequuntur perspiciatis itaque! Asperiores, ea dolor? Ab sunt, neque laboriosam laborum nisi quia magni quam explicabo enim officiis tempore delectus voluptates aut. Nesciunt aperiam quos velit dolores itaque molestiae eligendi. Fuga quia alias eum delectus placeat distinctio veritatis numquam repellendus necessitatibus, sed tempora autem nesciunt voluptates! Autem, ipsa. Fugiat, consequuntur itaque maxime maiores omnis cum quaerat rem numquam quos praesentium debitis incidunt ad cupiditate explicabo qui nisi laudantium.</p>
+        </div>
+    );
+};
+
+export default About;
